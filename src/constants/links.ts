6d0f987a681dc382fd5a5ea53679d53618d78bf7
da4thrza <alizadeh.reza@gmail.com>
2024-12-14 23:39:12 -0800

    first commit

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,6 +1,6 @@
 export const SOCIAL_LINKS = {
   x: 'https://twitter.com/blorbocoin',
-  dexscreener: 'https://dexscreener.com/blorbo',
+  dexscreener: 'https://dexscreener.com/base/0x3a91d3784415E64ae2Cf5c7a6c1D4d462dA7905A',
   website: 'https://blorbo.club',
   telegram: 'https://t.me/blorbocoin'
 };
